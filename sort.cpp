3ce7b2ed253c0b717a9915b89cd50607d5bbc1d5
zhangzcx61 <46404416+zhangzcx61@users.noreply.github.com>
2019-07-04 19:46:00 +0800

    Create sort.cpp

---
+++ sort.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+int main(){
+	using namespace std;
+	double area;
+	cin >> area;
+	double side;
+	side = sqrt(area);// double side = sqrt(area);
+	cout << side <<endl;
+	return 0;	
+	
+	
+	
+}
