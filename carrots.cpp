421013a3e652603212b57b993c22bfd0d5d22dab
zhangzcx61 <46404416+zhangzcx61@users.noreply.github.com>
2019-07-04 19:43:52 +0800

    Create carrots.cpp

---
+++ carrots.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+int main(){
+	using namespace std;
+	int carrots;
+	carrots = 25;
+	cout << "I have ";
+	cout << carrots;
+	cout << "carrots. ";
+	cout << endl;
+	carrots = carrots -1 ;
+	cout << carrots << endl;
+	return 0;
+	
+}
