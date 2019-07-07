5d2a04e74ceafdc8341e117739d658e7ce51049b
zhangzcx61 <46404416+zhangzcx61@users.noreply.github.com>
2019-07-07 20:53:20 +0800

    Create morechar.cpp

---
+++ morechar.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+int main(){
+	using namespace std;
+	char ch = 'M';
+	int i =ch;
+	//i = ch;
+	cout << ch << i << "(char and i )"<<endl;
+	cout.put(ch);
+	cout.put('!');
+	cout << endl <<"Done"<<endl;
+	return 0;
+}
