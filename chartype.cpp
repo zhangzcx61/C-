861a6e490f5490a3ae5a82993a14dd1c203cf514
zhangzcx61 <46404416+zhangzcx61@users.noreply.github.com>
2019-07-07 20:52:54 +0800

    Create chartype.cpp

---
+++ chartype.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+int main(){
+	using namespace std;
+	char ch;
+	cout << " Enter a character :"<<endl;
+	cin >> ch;
+	cout << " Thank you for the "<< ch<<" character."<<endl;
+	return 0;
+}
