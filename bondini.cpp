70afee187acc1f5694c8eed8c748511c11e78a3b
zhangzcx61 <46404416+zhangzcx61@users.noreply.github.com>
2019-07-07 20:53:46 +0800

    Create bondini.cpp

---
+++ bondini.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+int main(){
+	using namespace std;
+	cout << " \aOperation\"HyperHype\" is now activated! \n";
+	cout << " Enter your agent code :______\b\b\b\b\b\b\b\b";
+	long code;
+	cin >> code;
+	cout << " \aYou enterd"<< code << " ***\n";
+	cout << " \aCode verified ! Proceed with Plan Z3! \n";
+	return 0;
+	
+	
+	
+}
