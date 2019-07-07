9a976d575566ab535cb43ac2ea49c1ea1e4a69bc
zhangzcx61 <46404416+zhangzcx61@users.noreply.github.com>
2019-07-07 20:51:46 +0800

    Create hexoct.cpp

---
+++ hexoct.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+int main(){
+	using namespace std;
+	int chest = 42;
+	int waist = 0x42;
+	int inseam = 042;
+	cout << " MInsieur cuts a striking figure!\n"
+		 << " chest = "<< chest << " (42 in decimal)\n"
+		 << " waist = "<< waist << " ( 0x42 in hex)\n" 
+		 << " inseam ="<< inseam <<" ( 042 in octal)\n";
+		 return 0;
+	
+}
